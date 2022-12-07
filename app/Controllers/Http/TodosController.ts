17731791c785c56152e1c9c81a694864eb23ca1d
Nestor Recinos <nestor.recinos@airlinemro.parts>
2022-12-07 17:00:57 -0600

    :sparkles: Show information when added

--- app/Controllers/Http/TodosController.ts
+++ app/Controllers/Http/TodosController.ts
@@ -1,19 +1,17 @@
-import { Response } from '@adonisjs/core/build/standalone';
 import type { HttpContextContract } from '@ioc:Adonis/Core/HttpContext'
 
-import Todo from "App/Models/Todo";
+import Todo from 'App/Models/Todo'
 
 export default class TodosController {
   public async index(): Promise<Todo[]> {
-    return Todo.all();
+    return Todo.all()
   }
 
-  public async store({ request, response }: HttpContextContract) {
-    const newTodo = Todo.create({
+  public async store({ request, response }: HttpContextContract): Promise<void> {
+    const newTodo = await Todo.create({
       title: request.input('title'),
       is_completed: false,
-    });
-
-    return response.status(201).json(newTodo);
+    })
+    return response.status(201).send(newTodo)
   }
 }
